d1c7324bada350a1ce733fdd2027983f0c3738b4
Nik Nightingale <kot.nicko@yandex.ru>
2022-06-20 16:27:32 +0300

    Added 404-cases in views

--- notes/views.py
+++ notes/views.py
@@ -1,10 +1,9 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import *
 
 
 def projects(request):
-    projects_list = Project.objects.order_by('-date_update')
+    projects_list = get_list_or_404(Project.objects.order_by('-date_update'))
     context = {
         'projects_list': projects_list,
     }
@@ -13,8 +12,9 @@ def projects(request):
 
 def tasks(request, project_name):
     # tasks_list = Task.objects.order_by('-date_update')
-    project_id = Project.objects.get(name=project_name)
-    tasks_list = Task.objects.filter(project=project_id)
+    # project_id = get_object_or_404(Project.objects.get(name=project_name))
+    project_id = get_object_or_404(Project, name=project_name)
+    tasks_list = get_list_or_404(Task.objects.filter(project=project_id).order_by('-date_update'))
     context = {
         'tasks_list': tasks_list,
     }
@@ -23,8 +23,9 @@ def tasks(request, project_name):
 
 def notes(request, project_name, task_name):
     # notes_list = Note.objects.order_by('-date_update')
-    task_id = Task.objects.get(name=task_name)
-    notes_list = Note.objects.filter(task=task_id)
+    # task_id = get_object_or_404(Task.objects.get(name=task_name))
+    task_id = get_object_or_404(Task, name=task_name)
+    notes_list = get_list_or_404(Note.objects.filter(task=task_id).order_by('-date_update'))
     context = {
         'notes_list': notes_list,
     }
